df82ce4a3885405a9393eab25f8f7ea60d4d42a0
WimukthiPWijerathne <wimukthiwijerathne@gmail.com>
2025-04-29 13:53:12 +0530

    onebit full adder

---
+++ full adder/full_adder.v
@@ -0,0 +1,14 @@
+module fulladder(
+    inpput wire a,
+    input wire b,
+    input wire cin,
+    output wire cout,
+    output wire sum
+);
+
+    assgin {cout, sum} = a + b + cin;
+
+
+
+    endmodule
+    
