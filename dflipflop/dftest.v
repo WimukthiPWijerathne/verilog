f9add8e2ea87d0689501790bb22310473e0f2f90
WimukthiPWijerathne <wimukthiwijerathne@gmail.com>
2025-05-20 14:43:00 +0530

    did some changes

--- dflipflop/dftest.v
+++ dflipflop/dftest.v
@@ -16,15 +16,20 @@ module dtest;
     );
     initial begin
         clk = 0; // Initialize clock
-        #5 clk = ~clk; // Toggle clock every 5 time units
+        forever 
+            #5 clk = ~clk; // Toggle clock every 5 time units
+        
     end
 
     initial begin
         
-        #10 d = 0; clk = 0; en = 1; reset = 0; // Test case 1: d=0, clk=0, en=1, reset=0
-        #10 d = 1; clk = 1; en = 1; reset = 0; // Test case 2: d=1, clk=1, en=1, reset=0
-        #10 d = 0; clk = 0; en = 0; reset = 1; // Test case 3: d=0, clk=0, en=0, reset=1
-        #10 d = 1; clk = 1; en = 1; reset = 0; // Test case 4: d=1, clk=1, en=1, reset=0
+        #7 d = 0;  en = 1; reset = 0; // Test case 1: d=0, clk=0, en=1, reset=0
+        #5 d = 1;  en = 1; reset = 0; // Test case 2: d=1, clk=1, en=1, reset=0
+        #5 d = 0;  en = 0; reset = 1; // Test case 3: d=0, clk=0, en=0, reset=1
+        #5 d = 1;  en = 1; reset = 0; // Test case 4: d=1, clk=1, en=1, reset=0
+        #5 d = 1;  en = 1; reset = 0; // Test case 5: d=0, clk=0, en=1, reset=0
+        #5 d = 1;  en = 0; reset = 0; // Test case 6: d=1, clk=1, en=0, reset=0
+        $finish; // End the simulation
     end
     initial begin
         $dumpfile ("dflipflop.vcd");
